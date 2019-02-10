dbe2738866d59dbbca667c85c687c1d7d01608ec
Viktor Zhurbin <vzhurbin@gmail.com>
2019-02-10 22:31:26 +0300

    add company logo

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SVGInline from 'react-svg-inline';
+
+import logoSVG from '../assets/logo.svg';
+
+export default () => (
+    <div>
+        <SVGInline svg={logoSVG} />
+    </div>
+);
