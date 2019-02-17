4a60dbbf785b6bb5a1f6f126305d39151fed95c6
Viktor Zhurbin <vzhurbin@gmail.com>
2019-02-17 15:54:35 +0300

    connect stylelint

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,11 @@ module.exports = {
     plugins: {
         autoprefixer: {},
         stylelint: {},
-        'postcss-nested': {},
-        'postcss-import': {},
-        'postcss-mixins': {},
-        cssnano: {
-            preset: 'default',
-        },
+        // 'postcss-nested': {},
+        // 'postcss-import': {},
+        // 'postcss-mixins': {},
+        // cssnano: {
+        //     preset: 'default',
+        // },
     },
 };
