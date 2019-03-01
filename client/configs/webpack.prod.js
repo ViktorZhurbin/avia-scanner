12e10b14952501af98ddd4151b8d96068fd4928d
Viktor Zhurbin <vzhurbin@gmail.com>
2019-03-02 01:10:56 +0300

    profiler setup

--- client/configs/webpack.prod.js
+++ client/configs/webpack.prod.js
@@ -10,6 +10,10 @@ const outputDirectory = '../build';
 
 module.exports = merge(common, {
     mode: 'production',
+    alias: {
+        'react-dom$': 'react-dom/profiling',
+        'scheduler/tracing': 'scheduler/tracing-profiling',
+    },
     optimization: {
         splitChunks: {
             chunks: 'all',
