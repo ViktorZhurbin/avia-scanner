106342ad6dd58dfeae8451e683a90240ab7cc9cb
Viktor Zhurbin <vzhurbin@gmail.com>
2019-03-02 19:40:29 +0300

    set cleanWebpack root path for deploy

--- client/configs/webpack.prod.js
+++ client/configs/webpack.prod.js
@@ -1,12 +1,11 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
+const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 const common = require('./webpack.common.js');
-
-const outputDirectory = '../build';
 
 module.exports = merge(common, {
     mode: 'production',
@@ -16,7 +15,7 @@ module.exports = merge(common, {
         },
     },
     plugins: [
-        new CleanWebpackPlugin([outputDirectory]),
+        new CleanWebpackPlugin(['build'], { root: path.join(__dirname, '/..') }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
