06cc82d4f7de1ba61a5ff514313b703d5f529b81
Viktor Zhurbin <vzhurbin@gmail.com>
2019-03-03 23:05:53 +0300

    remove a string from value pushed to history

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -51,7 +51,7 @@ class App extends React.PureComponent {
     onSubmit = (event) => {
         event.preventDefault();
         const { searchObj, search } = this.getSearchQuery();
-        window.history.pushState(searchObj, '', `search${search}`);
+        window.history.pushState(searchObj, '', `${search}`);
         if (process.env.NODE_ENV === 'development') {
             this.fetchTickets(search); // for dev testing on mock data
         } else {
