d530d487a123909776fadfc5292637b0f00f92dc
Viktor Zhurbin <vzhurbin@gmail.com>
2019-03-13 12:36:24 +0300

    test Button

---
+++ client/__tests__/Button.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Button from '../src/components/Button';
+import Preloader from '../src/components/Preloader';
+
+describe('Render Button', () => {
+    it('render correctly with text children', () => {
+        const component = renderer.create(
+            <Button>
+                Find
+            </Button>,
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('render correctly with node children', () => {
+        const component = renderer.create(
+            <Button>
+                <div>Book</div>
+                <div>$156</div>
+            </Button>,
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('render correctly with Preloader', () => {
+        const component = renderer.create(
+            <Button isLoading>
+                <Preloader />
+            </Button>,
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
