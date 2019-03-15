a238682d14cc6f470aad061d4114aaf7d255441e
Viktor Zhurbin <vzhurbin@gmail.com>
2019-03-16 00:56:18 +0300

    use axios for currency fetch

--- server/controllers/currencyController.js
+++ server/controllers/currencyController.js
@@ -1,4 +1,4 @@
-const unirest = require('unirest');
+const axios = require('axios');
 
 const { mockRates } = require('../mockData/currencyRates');
 
@@ -6,7 +6,7 @@ const apiKey = '91ba9cf6354f4e83126b';
 const baseUrl = 'https://free.currencyconverterapi.com/api/v6/convert?';
 
 module.exports = {
-    fetchCurrencyRates: (req, res) => {
+    fetchCurrencyRates: async (req, res) => {
         const base = req.params.base;
         const currencyList = ['USD', 'EUR', 'RUB'];
         const requiredRates = currencyList.filter(item => item !== base);
@@ -19,23 +19,22 @@ module.exports = {
 
         const url = `${baseUrl}apiKey=${apiKey}&q=${queryOne},${queryTwo}&compact=ultra`;
 
-        unirest.get(url)
-            .end((result) => {
-                let rates = {};
-                if (result.ok) {
-                    Object.entries(result.body).map(([key, value]) => {
-                        const [baseCurrency, currency] = key.split('_');
-                        rates[currency] = value;
-                        rates[baseCurrency] = 1;
-                    });
-                } else {
-                    rates = mockRates(base);
-                }
+        let rates = {};
+        try {
+            const response = await axios.get(url);
+            // console.log(response.statusText, response.data);
+            Object.entries(response.data).map(([key, value]) => {
+                const [baseCurrency, currency] = key.split('_');
+                rates[currency] = value;
+                rates[baseCurrency] = 1;
+            });
+        } catch (error) {
+            console.log(error);
+            rates = mockRates(base);
+        }
 
-                res.json({
-                    body: rates,
-                });
-            })
-
+        res.json({
+            body: rates,
+        });
     }
 }
