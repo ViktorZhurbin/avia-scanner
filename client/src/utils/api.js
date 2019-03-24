df021d08bca0140cc6f1534189acad9e6292482c
Viktor Zhurbin <vzhurbin@gmail.com>
2019-03-24 10:16:18 +0300

    switch from axios to fetch

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,4 @@
 import qs from 'query-string';
-import axios from 'axios';
 
 const api = {
     createSession: 'api/createsession',
@@ -10,20 +9,23 @@ const api = {
 
 const fetchCurrencyRates = async (base) => {
     const encodedURI = window.encodeURI(`${api.getCurrencyRates}/${base}`);
-    const { data } = await axios(encodedURI);
+    const response = await fetch(encodedURI);
+    const data = await response.json();
 
     return data && data.body;
 };
 
 export const createApiSession = async (query) => {
     const encodedURI = window.encodeURI(`${api.createSession}/${query}`);
-    const { data } = await axios(encodedURI);
+    const response = await fetch(encodedURI);
+    const data = await response.json();
 
     return data && data.body;
 };
 
 export const fetchMockTicketData = async () => {
-    const { data } = await axios(api.mockData);
+    const response = await fetch(api.mockData);
+    const data = await response.json();
 
     return { ...data && data.body };
 };
@@ -43,7 +45,8 @@ export const fetchTickets = async (query) => {
         const currencyRates = await fetchCurrencyRates(currency);
 
         const ticketsURI = window.encodeURI(`${api.getTickets}/${sessionKey}`);
-        const { data } = await axios(ticketsURI);
+        const response = await fetch(ticketsURI);
+        const data = await response.json();
         const tickets = data && data.body;
 
         return { ...tickets, currencyRates };
