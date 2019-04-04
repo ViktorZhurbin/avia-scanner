d843226155889791cd03d9520378dc9413c60a2f
Viktor Zhurbin <vzhurbin@gmail.com>
2019-04-04 21:25:11 +0300

    remove unused props

--- client/src/containers/ResultsLayout/index.js
+++ client/src/containers/ResultsLayout/index.js
@@ -6,8 +6,6 @@ import { connect } from 'react-redux';
 import Filters from '../Filters';
 import TicketList from '../TicketList';
 
-import { getFilteredTickets } from '../../store/ticketData/selectors';
-
 import styles from './index.css';
 
 const cx = cl.bind(styles);
@@ -24,17 +22,13 @@ const ResultsLayout = ({ hasTickets }) => (
 );
 
 ResultsLayout.propTypes = {
-    hasTickets: PropTypes.bool,
-};
-
-ResultsLayout.defaultProps = {
-    hasTickets: false,
+    hasTickets: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = ({ tickets }) => ({
-    ticketData: tickets.ticketData,
     hasTickets: tickets.hasTickets,
-    filteredTickets: getFilteredTickets(tickets),
 });
 
-export default connect(mapStateToProps, null)(ResultsLayout);
+export default connect(
+    mapStateToProps,
+)(ResultsLayout);
