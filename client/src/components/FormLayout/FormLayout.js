9dd5be067bb0f63501ce7d65326cd57d9e81cf03
Viktor Zhurbin <vzhurbin@gmail.com>
2019-05-03 21:02:50 +0300

    hooks in FormLayout

--- client/src/components/FormLayout/FormLayout.js
+++ client/src/components/FormLayout/FormLayout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import cl from 'classnames/bind';
 
@@ -12,90 +12,90 @@ import styles from './FormLayout.css';
 
 const cx = cl.bind(styles);
 
-class FormLayout extends React.Component {
-    static propTypes = {
-        getTickets: PropTypes.func.isRequired,
-        getTicketsCancel: PropTypes.func.isRequired,
-        resetSearchQuery: PropTypes.func.isRequired,
-        resetTickets: PropTypes.func.isRequired,
-        setUpFormInput: PropTypes.func.isRequired,
-        isLoading: PropTypes.bool.isRequired,
-        hasTickets: PropTypes.bool,
-    };
-
-    static defaultProps = {
-        hasTickets: false,
-    };
-
-    componentDidMount() {
-        window.onpopstate = () => this.onUpdateState();
-        this.onUpdateState();
-    }
-
-    onUpdateState = () => {
-        const { getTickets, setUpFormInput } = this.props;
-        const { search } = window.location;
-        if (search.length > 0) {
-            getTickets(search);
-            setUpFormInput(search);
-        } else {
-            this.onResetState();
-        }
-    }
-
-    onResetState = () => {
-        const {
-            isLoading,
-            getTicketsCancel,
-            resetTickets,
-            setUpFormInput,
-            resetSearchQuery,
-        } = this.props;
-        window.history.pushState('', '', '/');
-        if (isLoading) {
-            ticketRequestController.abort();
-            getTicketsCancel();
-        }
-        resetTickets();
+const FormLayout = ({
+    getTickets,
+    getTicketsCancel,
+    resetSearchQuery,
+    resetTickets,
+    setUpFormInput,
+    isLoading,
+    hasTickets,
+}) => {
+    const updateSearch = () => {
         const lastSearchQuery = getLastSearchCookie();
         if (lastSearchQuery) {
             setUpFormInput(lastSearchQuery);
         } else {
             resetSearchQuery();
         }
-    }
-
-    onSubmit = (searchQuery) => {
-        const { getTickets } = this.props;
-        setLastSearchCookie(searchQuery);
-        getTickets(searchQuery);
-        window.history.pushState({ searchQuery }, '', searchQuery);
     };
 
-    render() {
-        const {
-            isLoading,
-            hasTickets,
-        } = this.props;
+    const cancelRequest = () => {
+        if (isLoading) {
+            ticketRequestController.abort();
+            getTicketsCancel();
+        }
+    };
 
-        return (
-            <div className={cx('container', { hasTickets })}>
-                <div className={cx('innerContainer', { hasTickets })}>
-                    <LoadingBar
-                        isLoading={isLoading}
-                    />
-                    <NavBar
-                        onResetState={this.onResetState}
-                    />
-                    <MainForm
-                        onSubmit={this.onSubmit}
-                        isLoading={isLoading}
-                        hasTickets={hasTickets}
-                    />
-                </div>
+    const onResetState = () => {
+        window.history.pushState('', '', '/');
+        cancelRequest();
+        resetTickets();
+        updateSearch();
+    };
+
+    const onLoad = (search) => {
+        if (search.length > 0) {
+            getTickets(search);
+            setUpFormInput(search);
+        } else {
+            onResetState();
+        }
+    };
+
+    const onSubmit = (search) => {
+        setLastSearchCookie(search);
+        getTickets(search);
+        window.history.pushState({ search }, '', search);
+    };
+
+    useEffect(() => {
+        const { search } = window.location;
+        window.onpopstate = () => onLoad(search);
+        onLoad(search);
+    }, [window.location.search]);
+
+    return (
+        <div className={cx('container', { hasTickets })}>
+            <div className={cx('innerContainer', { hasTickets })}>
+                <LoadingBar
+                    isLoading={isLoading}
+                />
+                <NavBar
+                    onResetState={onResetState}
+                />
+                <MainForm
+                    onSubmit={onSubmit}
+                    isLoading={isLoading}
+                    hasTickets={hasTickets}
+                />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+FormLayout.propTypes = {
+    getTickets: PropTypes.func.isRequired,
+    getTicketsCancel: PropTypes.func.isRequired,
+    resetSearchQuery: PropTypes.func.isRequired,
+    resetTickets: PropTypes.func.isRequired,
+    setUpFormInput: PropTypes.func.isRequired,
+    isLoading: PropTypes.bool.isRequired,
+    hasTickets: PropTypes.bool,
+};
+
+FormLayout.defaultProps = {
+    hasTickets: false,
+};
 
 export default FormLayout;
